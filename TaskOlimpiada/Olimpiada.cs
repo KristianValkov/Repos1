d6c23eb797e2fd5f134552fb5bf70bb1f588911b
KristianValkov <kv15497568@edu1.pgzaimov.com>
2023-02-22 12:55:07 +0200

    second commit

---
+++ TaskOlimpiada/Olimpiada.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskOlimpiada
+{
+    internal class Olimpiada:IPrint,IComparable<Olimpiada>
+    {
+        public string name;
+        public int countPoints;
+        public Olimpiada(string name, int countPoints)
+        {
+            this.name = name;
+            this.countPoints = countPoints;
+        }
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+        public int CountPoints
+        {
+            get { return this.countPoints; }
+            set { this.countPoints = value; }
+        }
+
+        public int CompareTo(Olimpiada other)
+        {
+           return countPoints.CompareTo(other.countPoints);
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"Name {this.name}, Points {this.countPoints}");
+        }
+
+    }
+}
---
+++ TaskOlimpiada/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskOlimpiada
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Olimpiada o1 = new Olimpiada();
+
+        }
+    }
+}
